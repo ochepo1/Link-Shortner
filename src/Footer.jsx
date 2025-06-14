c11a42bf1743cb0999ff77cceb48f1c24fa27edc
Ochepo Emmanuel <ochepoemmanuel16@gmail.com>
2025-06-14 01:17:16 +0100

    .

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
 
   return (
-    <div className="relative md:top-70 top-83 border-1 border-white w-[98%] min-h-10 flex items-center justify-center">
+    <div className="relative md:top-70 top-86 border-1 border-white w-[98%] min-h-10 flex items-center justify-center">
       <footer className="relative top-0">
         <div>Made by @ Ochepo Emmanuel 2025</div>
         <div>
